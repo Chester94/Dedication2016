a40d4e27fc9ca3adfa69eae894ac3e99e66738d6
Chester94 <Chester170394@mail.ru>
2016-08-11 09:04:54 +0300

    изменения в скрипте удаления вспомогательных файлов

--- clear.sh
+++ clear.sh
@@ -1,6 +1,13 @@
-rm -rf *.log
-rm -rf *.aux
-rm -rf *.out
-rm -rf *.pdf
-rm -rf *.gz
-rm -rf *.toc
+rm -rf ./*.log
+rm -rf ./*.aux
+rm -rf ./*.out
+rm -rf ./*.pdf
+rm -rf ./*.gz
+rm -rf ./*.toc
+
+rm -rf ./scenario/*.log
+rm -rf ./scenario/*.aux
+rm -rf ./scenario/*.out
+rm -rf ./scenario/*.pdf
+rm -rf ./scenario/*.gz
+rm -rf ./scenario/*.toc
