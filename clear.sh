934a357d28987d4135e531c6b2527322103ff78c
Chester94 <Chester170394@mail.ru>
2016-08-11 09:22:11 +0300

    добавлена очистка папки Расписания

--- clear.sh
+++ clear.sh
@@ -11,3 +11,10 @@ rm -rf ./scenario/*.out
 rm -rf ./scenario/*.pdf
 rm -rf ./scenario/*.gz
 rm -rf ./scenario/*.toc
+
+rm -rf ./schedule/*.log
+rm -rf ./schedule/*.aux
+rm -rf ./schedule/*.out
+rm -rf ./schedule/*.pdf
+rm -rf ./schedule/*.gz
+rm -rf ./schedule/*.toc
