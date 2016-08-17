83c7fab5f25e06ca863501878c968a6ca0b6e7f0
Chester94 <Chester170394@mail.ru>
2016-08-17 16:58:19 +0300

    Скрипт очистки стал еще лаконичнее и правильнее

--- clear.sh
+++ clear.sh
@@ -1,22 +1,5 @@
 #!/bin/bash 
 
-function clean {
-    cleanFolder
-    for i in $( find . -type d ! -wholename "."| grep -v ".*\.git.*" | cut -d '/' -f2 ); do
-        cd $i
-        clean
-        cd ..
-    done
-}
-
-function cleanFolder {
-    rm -rf ./*.log
-    rm -rf ./*.aux
-    rm -rf ./*.out
-    rm -rf ./*.pdf
-    rm -rf ./*.gz
-    rm -rf ./*.toc
-    echo `pwd` cleared
-}
-
-clean
+for i in $( find . -type f -regex ".*\.log\|.*\.aux\|.*\.out\|.*\.pdf\|.*\.gz\|.*\.toc" ); do
+    rm $i
+done
